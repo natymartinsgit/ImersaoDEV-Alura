dd62ec7524b3013f73bcd0b9d38977e714e80a48
natymartins <naty_martins01@zohomail.com>
2024-10-09 15:59:12 -0300

    Adicionei alguns conteúdo
    adicionei apps.js, dados.js e o styles.css

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      titulo: "Minimalista",
+      descricao: "minimalismo é uma ótima opção para quem busca um estilo elegante, prático e atemporal. É uma forma de expressão pessoal que valoriza a qualidade e a simplicidade, tornando o processo de se vestir mais fácil e prazeroso.",
+      link: "https://www.doralis.com.br/moda-minimalista-tudo-que-voce-precisa-saber-sobre-esse-estilo"
+    },
+    {
+      titulo: "Vintage",
+      descricao: "O estilo vintage resgata a moda de épocas passadas, trazendo um ar nostálgico e cheio de charme. É perfeito para quem gosta de peças únicas e com história.",
+      link: "https://www.dicasdemulher.com/estilo-vintage/"
+    },
+    {
+      titulo: "Romântico",
+      descricao: "O estilo romântico é marcado pela delicadeza, feminilidade e leveza. Rendas, babados e cores suaves são características marcantes desse estilo.",
+      link: "https://www.escoladeestilo.com.br/estilo-romantico/"
+    }
+  ];
