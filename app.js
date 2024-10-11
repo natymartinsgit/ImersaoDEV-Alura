df7a6f645526fac86cc01bb54327d63a7b31fc04
natymartins <naty_martins01@zohomail.com>
2024-10-11 12:22:41 -0300

    refactor: Para melhorias no código sem alterações na funcionalidade.

--- app.js
+++ app.js
@@ -2,49 +2,33 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
-    // Obtém o valor do campo de pesquisa
-    let campoPesquisa = document.getElementById("campoPesquisa").value;
+let campoPesquisa = document.getElementById
+("campoPesquisa")
 
-    // Exibe o valor no console para depuração
-    console.log(campoPesquisa);
-
-    // Verifica se o campo de pesquisa está vazio
-    if (!campoPesquisa) {
-        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar algo.</p>";
-        return;
+    // Se campoPesquisa for uma string sem nada
+    if(campoPesquisa = "") {
+        section.innerHTML = "Nada foi encontrado"
+        return
     }
-
-    // Converte o valor do campo de pesquisa para minúsculas
-    campoPesquisa = campoPesquisa.toLowerCase();
-
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
 
     // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        // Converte os títulos, descrições e tags para minúsculas
-        let titulo = dado.titulo.toLowerCase();
-        let descricao = dado.descricao.toLowerCase();
-        let tags = dado.tags ? dado.tags.toLowerCase() : ""; // Adiciona tags se existirem
-
-        // Verifica se o termo pesquisado está no título, descrição ou tags
-        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
-            // Cria um novo elemento HTML para cada resultado
+       
+        // se titulo icludes campoPesquisa
+        if(dado.titulo.includes(campoPesquisa)){
+          
+            // cria um novo elemento
             resultados += `
-                <div class="item-resultado">
-                    <h2>
-                        <a href="${dado.link}" target="_blank">${dado.titulo}</a>
-                    </h2>
-                    <p class="descricao-meta">${dado.descricao}</p>
-                    <a href="${dado.link}" target="_blank">Mais informações</a>
-                </div>
-            `;
+            <div class="item-resultado">
+                <h2>
+                    <a href="${dado.link}" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+            </div>
+        `;
         }
-    }
-
-    // Exibe uma mensagem se não houver resultados
-    if (!resultados) {
-        resultados = "<p>Nada foi encontrado</p>";
     }
 
     // Atribui os resultados gerados à seção HTML
