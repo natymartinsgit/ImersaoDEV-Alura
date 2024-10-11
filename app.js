367fe0ba758b437077bf6c66134c58c14c76ecae
natymartins <naty_martins01@zohomail.com>
2024-10-11 13:47:59 -0300

    FIX: Arrumei o botão pesquisar

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
 
-let campoPesquisa = document.getElementById
-("campoPesquisa")
+  let campoPesquisa = document.getElementById("campoPesquisa").value;
 
-    // Se campoPesquisa for uma string sem nada
-    if(campoPesquisa = "") {
-        section.innerHTML = "Nada foi encontrado"
-        return
-    }
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-
-    // Itera sobre cada dado da lista de dados
-    for (let dado of dados) {
-       
-        // se titulo icludes campoPesquisa
-        if(dado.titulo.includes(campoPesquisa)){
-          
-            // cria um novo elemento
-            resultados += `
+  // Se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado.</p>";
+    return;
+  }
+  campoPesquisa = campoPesquisa.toLowerCase();
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let link = "";
+  let tags = "";
+  // Itera sobre cada dado da lista de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    // se titulo icludes campoPesquisa
+    if (
+      titulo.includes(campoPesquisa) ||
+      descricao.includes(campoPesquisa) ||
+      tags.includes(campoPesquisa)
+    ) {
+      // cria um novo elemento
+      resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="${dado.link}" target="_blank">${dado.titulo}</a>
@@ -28,9 +36,21 @@ let campoPesquisa = document.getElementById
                 <p class="descricao-meta">${dado.descricao}</p>
             </div>
         `;
-        }
     }
+  }
+  if (!resultados) {
+    resultados = "<p>Não encontrado</p>";
+  }
 
-    // Atribui os resultados gerados à seção HTML
-    section.innerHTML = resultados;
+  // Atribui os resultados gerados à seção HTML
+  section.innerHTML = resultados;
+}
+
+function limpar() {
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campoPesquisa");
+
+
+  campoPesquisa.value = ""
+  section.innerHTML = ""
 }
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
       titulo: "Minimalista",
       descricao: "minimalismo é uma ótima opção para quem busca um estilo elegante, prático e atemporal. É uma forma de expressão pessoal que valoriza a qualidade e a simplicidade, tornando o processo de se vestir mais fácil e prazeroso.",
-      link: "https://www.doralis.com.br/moda-minimalista-tudo-que-voce-precisa-saber-sobre-esse-estilo"
+      link: "https://www.doralis.com.br/moda-minimalista-tudo-que-voce-precisa-saber-sobre-esse-estilo",
+      tags: "Estilo elegante mini"
     },
     {
       titulo: "Vintage",
       descricao: "O estilo vintage resgata a moda de épocas passadas, trazendo um ar nostálgico e cheio de charme. É perfeito para quem gosta de peças únicas e com história.",
-      link: "https://www.dicasdemulher.com/estilo-vintage/"
+      link: "https://www.dicasdemulher.com/estilo-vintage/",
+      tags: "nostágio vinta moda época passadas"
     },
     {
       titulo: "Romântico",
       descricao: "O estilo romântico é marcado pela delicadeza, feminilidade e leveza. Rendas, babados e cores suaves são características marcantes desse estilo.",
-      link: "https://www.escoladeestilo.com.br/estilo-romantico/"
+      link: "https://www.escoladeestilo.com.br/estilo-romantico/", 
+      tags: "delicadeza romantico român estilo leve roman"
     }
   ];
